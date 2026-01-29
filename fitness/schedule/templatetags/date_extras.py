919464e2850a1df707c673727ae892e789cae392
Vladislav Moskalchuk <vlad3002vm.76@gmail.com>
2026-01-29 18:47:48 +0300

    the format of the class day date has been changed

---
+++ fitness/schedule/templatetags/date_extras.py
@@ -0,0 +1,24 @@
+from datetime import date, datetime
+
+from django import template
+from django.utils import timezone
+
+from dateutil.relativedelta import relativedelta
+from django.utils.formats import date_format
+
+register = template.Library()
+
+@register.filter
+def to_day(day: date | datetime):
+    now = timezone.localtime(timezone.now()).date()
+
+    if day == now:
+        return "сегодня"
+    elif day == now - relativedelta(days=1):
+        return "вчера"
+    elif day == now + relativedelta(days=1):
+        return "завтра"
+    elif day.year == now.year:
+        return date_format(day, "l, j E").lower()
+    else:
+        return date_format(day, "j E Y")
